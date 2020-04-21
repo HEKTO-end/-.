be82a507a4ced43dcacc3e1aa44754b9cdaca240
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2020-04-21 13:32:14 +0400

    Я сделал
    вроде всё верно

---
+++ description of item/description of item/ball.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace description_of_item
+{
+    class ball
+    {
+        public int weight { get; set; }
+        public string type_of_balls { get; set; }
+        public string the_size_of_the_ball { get; set; }
+        public int razmer { get; set; }
+
+
+    }
+}
