087ed3d05a2b2e8ec17a2e881546bb4b47bca409
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2020-04-22 14:34:26 +0400

    ГОВНОКОД)

---
+++ description of item/ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace description_of_item
+{
+    class ball
+    {
+        public int weight { get; set; }
+        public string type_of_balls { get; set; }
+        public string the_size_of_the_ball { get; set; }
+        public int razmer { get; set; }
+        public double speed { get; set; }
+        public string material { get; set; }
+
+        string metal = "Металл";
+        int HP = 100;
+        public void Damag()
+        {
+            if (speed > 10)
+            {
+                HP -= 5;
+            }
+            else
+            {
+                smert();
+            }
+        }
+        public string smert()
+        {
+            if (material == metal)
+            {
+                return "ТЫ УМЕР!";
+            }
+            return "";
+        }
+    }
+}
